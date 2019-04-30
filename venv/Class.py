25bcdc5e0cd342d944f73847a29a1f929ff9a9fd
yyl <yyl7727@github.com>
2019-04-30 09:23:13 +0800

    1.0
    第一次

---
+++ venv/Class.py
@@ -0,0 +1,36 @@
+#编写一个名为 collatz()的函数，它有一个名为 number 的参数。如果参数是偶数，
+#那么 collatz()就打印出 number // 2，并返回该值。如果 number 是奇数，collatz()就打
+#印并返回 3 * number + 1。
+def Collatz(number):
+    if number==1:
+        return number
+    elif number%2==0:
+        return number/2
+    else:
+        return 3*number+1
+
+#传入一个列表，返回一个字符串，该字符串包含所
+#有表项，表项之间以逗号和空格分隔，并在最后一个表项之前插入 and。
+def ListToString(spam):
+    str2=''
+    for i in range(len(spam)-1):
+        str2=str2+str(spam[i])+','
+    str2=str2+'and '+spam[-1]
+    return str2
+
+#漂亮的输出一个桃心
+def beautifulprint():
+    grid = [[' ', ' ', ' ', ' ', ' ', ' '],
+            [' ', 'O', 'O', ' ', ' ', ' '],
+            ['O', 'O', 'O', 'O', ' ', ' '],
+            ['O', 'O', 'O', 'O', 'O', ' '],
+            [' ', 'O', 'O', 'O', 'O', 'O'],
+            ['O', 'O', 'O', 'O', 'O', ' '],
+            ['O', 'O', 'O', 'O', ' ', ' '],
+            [' ', 'O', 'O', ' ', ' ', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ']]
+
+    for i in range(len(grid[0])):
+        for j in range(len(grid)):
+            print(grid[j][i],end='')
+        print()
---
+++ venv/Player.py
@@ -0,0 +1,24 @@
+import Class
+
+operation=int(input())
+
+if operation==1:
+    try:
+        num=int(input())
+    except exception:
+        print("请输入一个整数")
+    while True:
+        flag=int(Class.Collatz(num))
+        if flag!=1:
+            num=flag
+            print(flag)
+        else:
+            print(flag)
+            break
+
+elif operation==2:
+    spam=['yyl','zxy','yyl','zxy']
+    print(Class.ListToString(spam))
+
+elif operation==3:
+    Class.beautifulprint()
